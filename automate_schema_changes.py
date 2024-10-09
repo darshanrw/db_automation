45f33035b727ff00d30ae302238b63816993f7e1
darshanrw <Dwadekar4575@conestogac.on.ca>
2024-10-08 23:06:56 -0400

    updated python file

--- automate_schema_changes.py
+++ automate_schema_changes.py
@@ -1,41 +1,6 @@
 # Full name: Darshan Wadekar 
 # Student ID: 8934575 
 # Class number: Fall Section 1 - Cloud DevOps
-"""
-import pymysql
-
-# Create the database connection directly
-connection = pymysql.connect(
-    host='localhost',  
-    user='root',  
-    password='Secret55',  
-    database='sampledb'
-)
-
-
-
-# Path to the SQL file
-script_path = 'add_departments.sql'
-
-# Read the SQL script from the file
-with open(script_path, 'r') as file:
-    sql_script = file.read()
-
-# Create a cursor object to execute SQL queries
-cursor = connection.cursor()  
-
-# Execute each SQL statement individually (split by semicolon)
-for statement in sql_script.split(';'):
-    if statement.strip():  # Execute only non-empty statements
-        cursor.execute(statement)
-
-connection.commit()  # Commit all changes at once
-cursor.close()  # Close the cursor
-connection.close()  # Close the connection
-
-print("Execution completed successfully")
-
-"""
 
 import mysql.connector
 import os
